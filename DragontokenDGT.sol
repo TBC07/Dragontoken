59963d1bd71ffa40ac517e35e10eb5e245822502
TBC07 <87712777+TBC07@users.noreply.github.com>
2023-05-31 21:52:03 +0530

    Add files via upload

---
+++ DragontokenDGT.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+     string public TokenName = "Dragontoken";
+     string public TokenAbbrv = "DGT";
+     uint256 public Totalsupply = 0;
+    // mapping variable here
+
+    mapping (address => uint256) Accountvalue;
+
+
+    // mint function
+    function Mint(address _address , uint _Balance) public {
+        Totalsupply += _Balance;
+        Accountvalue[_address] += _Balance;
+
+    }
+
+    // burn function
+    function Burn(address _address , uint _Balance) public {
+        if (Accountvalue[_address] >= _Balance){
+        Totalsupply -= _Balance;
+        Accountvalue[_address] -= _Balance;
+      }
+
+    }
+}
